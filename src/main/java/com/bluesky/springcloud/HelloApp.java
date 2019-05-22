b5d363c208d01d33a31fc3b18d9956fb80d4dc4a
zhanghqchina <zhanghqchina@163.com>
2019-05-22 12:31:46 +0800

    second commit HelloApp

--- src/main/java/com/bluesky/springcloud/HelloApp.java
+++ src/main/java/com/bluesky/springcloud/HelloApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApp {
     public static void main(String[] args) {
+        // seconde commit use
         SpringApplication.run(HelloApp.class, args);
     }
 }
