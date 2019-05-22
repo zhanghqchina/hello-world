dd93235a25c469ab448e5c375e43b63be4709338
zhanghqchina <zhanghqchina@163.com>
2019-05-22 12:51:54 +0800

    测试代码冲突

--- src/main/java/com/bluesky/springcloud/HelloApp.java
+++ src/main/java/com/bluesky/springcloud/HelloApp.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApp {
     public static void main(String[] args) {
-        // seconde commit use
-        // 主启动类
         SpringApplication.run(HelloApp.class, args);
     }
 }
