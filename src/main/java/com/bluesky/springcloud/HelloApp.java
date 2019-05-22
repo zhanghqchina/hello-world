3d2ff7cf582709825d97b9c0287fc647c148f209
zhanghqchina <zhanghqchina@163.com>
2019-05-22 12:42:04 +0800

    Update HelloApp.java

--- src/main/java/com/bluesky/springcloud/HelloApp.java
+++ src/main/java/com/bluesky/springcloud/HelloApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApp {
     public static void main(String[] args) {
         // seconde commit use
+        // 主启动类
         SpringApplication.run(HelloApp.class, args);
     }
 }
