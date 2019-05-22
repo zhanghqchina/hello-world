3c810376630b4030985560b52a8ca9a2b0fe8485
zhanghqchina <zhanghqchina@163.com>
2019-05-22 12:53:10 +0800

    github 上的修改
    github 上的修改

--- src/main/java/com/bluesky/springcloud/HelloApp.java
+++ src/main/java/com/bluesky/springcloud/HelloApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApp {
     public static void main(String[] args) {
+        // github 上的修改
         SpringApplication.run(HelloApp.class, args);
     }
 }
